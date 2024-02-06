0add351b1a2774057439c12a105fa3778b460b08
ConLockyer <145501505+ConLockyer@users.noreply.github.com>
2024-02-06 14:01:25 -0500

    code youtuh practice

---
+++ Day eight/products.js
@@ -0,0 +1,21 @@
+[
+    {
+        "id": 1,
+        "name": "LD01 Drawing Tablet",
+        "price": 200,
+        "image": "img/1.jpg"
+    },
+    {
+        "id": 2,
+        "name": "LD02 Drawing Glove",
+        "price": 200,
+        "image": "img/2.jpg"
+    },
+    {
+        "id": 3,
+        "name": "LD03 Drawing Tablet Stand w/Glove",
+        "price": 200,
+        "image": "img/3.jpg"
+    }
+
+]
---
+++ Day seven/day seven.js
@@ -0,0 +1,7 @@
+const body = document.body
+
+const div = document.createElement("div")
+
+div.innText = "hello world"
+
+document.append(div)
